67e68441f6643ad0a661d443a3cb6e63388b6597
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-28 08:43:12 -0500

    added animation for hero logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       />
       {/* Optional Overlay to make text readable */}
       <div className="absolute flex h-full w-full flex-col items-center justify-center gap-3 bg-black/40 px-16">
-        <div className="rounded-sm border px-12 font-bungee text-6xl font-bold text-white shadow-lg sm:text-9xl">
+        <div className="rounded-sm border px-12 font-bungee text-6xl font-bold text-white shadow-lg transition-colors duration-300 ease-in-out select-none hover:bg-white/40 hover:text-black sm:text-9xl">
           ONYX
         </div>
         <div className="font-lexend text-lg font-bold text-white italic sm:text-[33px]">
