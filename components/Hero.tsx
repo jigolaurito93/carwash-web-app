aef8b6f199c6768ddcecbfccf963fee66d5c5b94
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-28 05:36:39 -0500

    Designing hero logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,8 +11,14 @@ const Hero = () => {
         className="object-cover object-top"
       />
       {/* Optional Overlay to make text readable */}
-      <div className="absolute inset-0 flex items-center bg-black/30 px-16">
-        <h2 className="text-8xl font-bold text-white">Onyx Premium Carwash</h2>
+      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-3 bg-black/30 px-16 md:items-start">
+        <div className="rounded-sm bg-black px-10 py-2 text-8xl font-bold text-white shadow-lg sm:text-9xl md:text-[170px]">
+          ONYX
+        </div>
+        <div className="flex gap-3 px-5 py-2 text-4xl font-bold text-white md:text-[80px]">
+          <div className="">Premium</div>
+          <div className="">Carwash</div>
+        </div>
       </div>
     </section>
   );
