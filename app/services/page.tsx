6a7e2e6aa6625f430615fd73221294a0c402d043
Jose Laurito <jigolaurito93@yahoo.com>
2026-03-01 09:39:47 -0500

    changed mobile view text size

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -16,11 +16,10 @@ import ServicesCard2 from "@/components/services/ServicesCard2";
 const ServicesPage = () => {
   return (
     <div className="flex flex-col px-4 pt-20 pb-32">
-      <div className="mx-auto py-10 font-lexend text-lg">
+      {/* Services and Pricing */}
+      <div className="mx-auto py-10 font-lexend text-xl md:text-3xl">
         Services and Pricing
       </div>
-
-      {/* Services and Pricing */}
       <div className="flex flex-wrap items-stretch justify-center gap-7">
         <ServicesCard2 serviceCard={servicesList[0]} />
         <ServicesCard2 serviceCard={servicesList[1]} />
@@ -29,21 +28,20 @@ const ServicesPage = () => {
       </div>
       {/* Services and Pricing End */}
 
-      <div className="mx-auto py-10 font-lexend text-lg">Other Services</div>
-      <div className="item-stretch flex flex-wrap justify-center gap-7">
-        {/* Add-Ons List */}
-        <ServicesCard serviceCard={addOnsList} />
-        {/* Paint Protection List */}
-        <ServicesCard serviceCard={paintProtectionList} />
-        {/* Hand Wax List */}
-        <ServicesCard serviceCard={waxList} />
-        {/* Engine Bay Cleaning List */}
-        <ServicesCard serviceCard={engineBayCleaningList} />
-        {/* Detailing List */}
-        {/* <ServicesCard serviceCard={detailList} /> */}
+      {/* Other Services */}
+      <div className="mx-auto py-10 font-lexend text-xl md:text-3xl">
+        Other Services
       </div>
+      <div className="item-stretch flex flex-wrap justify-center gap-7">
+        <ServicesCard serviceCard={addOnsList} />
+        <ServicesCard serviceCard={paintProtectionList} />
+        <ServicesCard serviceCard={waxList} />
+        <ServicesCard serviceCard={engineBayCleaningList} />
+      </div>
+      {/* Other Services End */}
 
-      <div className="mx-auto py-10 font-lexend text-lg">
+      {/* Detailing Services */}
+      <div className="mx-auto py-10 font-lexend text-xl md:text-3xl">
         Detailing Services
       </div>
       <div className="item-stretch flex flex-wrap justify-center gap-7">
@@ -52,6 +50,7 @@ const ServicesPage = () => {
         <ServicesCard2 serviceCard={exteriorDetailList} />
         <ServicesCard2 serviceCard={miniDetailList} />
       </div>
+      {/* Detailing Services End */}
     </div>
   );
 };
