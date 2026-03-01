72f6f4b988793ed72b33996997cb58c12efeaba8
Jose Laurito <jigolaurito93@yahoo.com>
2026-03-01 09:32:53 -0500

    refactored service section on home page

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -1,6 +1,6 @@
-import { MdLocalCarWash } from "react-icons/md";
 import Link from "next/link";
 import { servicesList } from "@/data/services";
+import ServicesCard2 from "./services/ServicesCard2";
 
 const ServicesSection = () => {
   return (
@@ -8,35 +8,13 @@ const ServicesSection = () => {
       <div className="pt-10 text-center font-lexend text-3xl">
         Services and Pricing
       </div>
-      <div className="mx-auto my-4 w-fit rounded-sm bg-black px-5 py-3 text-center font-questrial text-xl font-bold text-white lg:px-10 lg:text-2xl">
+      <div className="mx-auto my-4 w-fit rounded-sm bg-black px-9 py-1 text-center font-questrial text-xl font-bold text-white lg:px-24 lg:text-2xl">
         Starting at $14.99
       </div>
       <div className="flex flex-wrap items-stretch justify-center gap-7">
-        {servicesList.map((service) => (
-          // Card
-          <div
-            key={service.title}
-            className="flex w-full max-w-80 flex-col gap-5 rounded-md border border-black/30 p-4 pb-10 shadow-lg xl:pb-10"
-          >
-            <div className="shrink-0 rounded-md bg-black/10 p-2 text-black">
-              <div className="text-center font-lexend text-xl">
-                {service.title}
-              </div>
-              <div className="text-center font-lexend text-sm">
-                {service.description}
-              </div>
-            </div>
-            <div className="flex min-h-0 flex-1 flex-col gap-2 pl-2">
-              {service.features.map((feature) => (
-                <div key={feature} className="flex items-center gap-3">
-                  <MdLocalCarWash className="shrink-0 text-xl text-blue-900" />
-                  <div className="font-questrial text-sm">{feature}</div>
-                </div>
-              ))}
-            </div>
-          </div>
-          // Card End
-        ))}
+        <ServicesCard2 serviceCard={servicesList[0]} />
+        <ServicesCard2 serviceCard={servicesList[1]} />
+        <ServicesCard2 serviceCard={servicesList[2]} />
       </div>
       <div className="mx-auto mt-10 w-fit cursor-pointer rounded-md border border-black/50 px-7 py-3 text-center font-lexend text-sm shadow-2xl transition-colors duration-300 hover:bg-black/50">
         <Link href="/services">See All Services</Link>
