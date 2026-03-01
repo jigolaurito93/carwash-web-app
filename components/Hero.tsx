b550d75b555cc0ddd2c097f6880dd3c534d26945
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-28 21:42:14 -0500

    Added catchphrase from brand

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="relative h-screen w-full overflow-hidden">
+    <div className="relative h-screen w-full overflow-hidden">
       <Image
         src="/images/carwash-1.jpg"
         alt="Carwash Image"
@@ -11,15 +11,18 @@ const Hero = () => {
         className="object-cover object-top"
       />
       {/* Optional Overlay to make text readable */}
-      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-3 bg-black/40 px-16">
+      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-2 bg-black/40 px-12">
         <div className="rounded-sm border px-12 font-bungee text-6xl font-bold text-white shadow-lg transition-colors duration-300 ease-in-out select-none hover:bg-white/40 hover:text-black sm:text-9xl">
           ONYX
         </div>
         <div className="font-lexend text-lg font-bold text-white italic sm:text-[33px]">
           PREMIUM CARWASH
         </div>
+        <div className="text-center font-lexend text-sm text-white sm:text-lg lg:text-xl">
+          &rdquo;The Gold Standard of Clean&rdquo;
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
