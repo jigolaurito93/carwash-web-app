485eb69724a49997713c20bb4a820c8935d15c02
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-28 23:56:15 -0500

    Created data for services

---
+++ data/services.ts
@@ -0,0 +1,33 @@
+export const servicesList = [
+  {
+    title: "Regular Wash",
+    description: "",
+    features: [
+      "Hand Exterior Wash & Dry",
+      "Basic Interior Vacuum (Floors & Seats)",
+      "Window Cleaning (Inside & Out)",
+      "Dusting of Dashboard",
+    ],
+  },
+  {
+    title: "Premium Wash",
+    description: "(Most Popular)",
+    features: [
+      "Everything in Regular Wash, plus:",
+      "Spray Wax Protection",
+      "Tires Armor All & Rim Cleaning",
+      "Air Freshener (Choice of Scent)",
+    ],
+  },
+  {
+    title: "Premium+ Wash",
+    description: "(The Onyx Signature)",
+    features: [
+      "Everything in Premium Wash, plus:",
+      "Conditioning of Leather Seats OR Dash & Doors",
+      "Deep Carpet Scrub (Floor Mats)",
+      "Clay Bar Treatment (Exterior Paint Smoothness)",
+      "Rain-X Windshield Treatment",
+    ],
+  },
+];
