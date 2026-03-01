a87fdce5981ca9493c763c8756a580c2520937af
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-28 23:57:11 -0500

    created the service section of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import ServicesSection from "@/components/ServicesSection";
 import Welcome from "@/components/Welcome";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <ServicesSection />
       <Welcome />
     </div>
   );
---
+++ components/ServicesSection.tsx
@@ -0,0 +1,44 @@
+import { MdLocalCarWash } from "react-icons/md";
+import Link from "next/link";
+import { servicesList } from "@/data/services";
+
+const ServicesSection = () => {
+  return (
+    <div className="px-6 pt-6 pb-16 font-questrial md:px-24 lg:px-40">
+      <div className="py-10 text-center font-lexend text-3xl">
+        Services and Pricing
+      </div>
+      <div className="flex flex-wrap items-stretch justify-center gap-7">
+        {servicesList.map((service) => (
+          // Card
+          <div
+            key={service.title}
+            className="flex w-full max-w-80 flex-col gap-5 rounded-md border border-black/30 p-4 shadow-lg xl:pb-10"
+          >
+            <div className="shrink-0 rounded-md bg-black/10 p-2 text-black">
+              <div className="text-center font-lexend text-xl">
+                {service.title}
+              </div>
+              <div className="text-center font-lexend text-sm">
+                {service.description}
+              </div>
+            </div>
+            <div className="flex min-h-0 flex-1 flex-col gap-2 pl-2">
+              {service.features.map((feature) => (
+                <div key={feature} className="flex items-center gap-3">
+                  <MdLocalCarWash className="shrink-0 text-lg" />
+                  <div className="font-questrial text-sm">{feature}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="mx-auto mt-10 w-fit cursor-pointer rounded-md border border-black/50 px-7 py-3 text-center font-lexend text-sm shadow-2xl transition-colors duration-300 hover:bg-black/50">
+        <Link href="/services">See All Services</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
