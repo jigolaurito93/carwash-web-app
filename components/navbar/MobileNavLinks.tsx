158b9be2c758f6b9522d84b1fd8f22907d415440
jigolaurito93 <jigolaurito93@yahoo.com>
2026-03-02 03:56:06 -0600

    create a function to close mobile navbar when route is selected

--- components/navbar/MobileNavLinks.tsx
+++ components/navbar/MobileNavLinks.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SetStateAction, Dispatch } from "react";
 import { IoMdCloseCircle } from "react-icons/io";
 
@@ -6,19 +7,32 @@ interface MobileNavProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 const MobileNaveLinks = ({ isOpen, setIsOpen }: MobileNavProps) => {
+  //Function to close the navbar when a route is clicked
+  const handleCloseNav = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div
       className={`fixed top-0 left-0 z-50 h-screen w-screen bg-black text-white transition-transform duration-300 lg:hidden ${isOpen ? "translate-x-0" : "translate-x-full"}`}
     >
       <IoMdCloseCircle
         className="absolute top-8 left-8 cursor-pointer text-4xl text-white"
-        onClick={() => setIsOpen(false)} // Sets it specifically to closed
+        onClick={handleCloseNav} // Sets it specifically to closed
       />
       <div className="flex h-full w-full flex-col items-center justify-center gap-16 font-lexend text-4xl font-medium">
-        <div>Home</div>
-        <div>About</div>
-        <div>Services</div>
-        <div>Home</div>
+        <Link href={"/"} onClick={handleCloseNav}>
+          Home
+        </Link>
+        <Link href={"/about"} onClick={handleCloseNav}>
+          About
+        </Link>
+        <Link href={"/services"} onClick={handleCloseNav}>
+          Services
+        </Link>
+        <Link href={"/"} onClick={handleCloseNav}>
+          Home
+        </Link>
       </div>
     </div>
   );
